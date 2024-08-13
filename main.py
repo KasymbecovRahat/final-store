f755c10f72364c58714ce88a60ff588743882caa
KasymbecovRahat <kasymbecovrahat@gmail.com>
2024-08-13 13:50:21 +0600

    new project

---
+++ main.py
@@ -0,0 +1,93 @@
+# numbers = [6, 7, 3, 2]
+# min_numbers = numbers[0]
+# for min in numbers:
+#     min_numbers >= min
+# print(min)
+
+# numbers =[6,7,3,2]
+# max_numbers = 0
+# for max in numbers:
+#     if max > max_numbers:
+#         max_numbers = max
+# print(max_numbers)
+
+# words =input('enter:')
+# count = int(input('count:'))
+# for i in range(count):
+#     print(words)
+
+# cars = {'car_name': 'mers', 'model': 'AMG', 'price': 50000}
+# word = input('write a key: ')
+#
+# if word in cars:
+#     print(cars[word])
+# else:
+#     print('Error')
+
+
+# words = input('write your name:')
+# for i in words:
+#     print(i)
+
+# numbers = [6, 7, 3, 65]
+#
+# max_number = numbers[0]
+#
+# for i in numbers[1:]:
+#     if i > max_number:
+#         max_number = i
+# print(max_number)
+
+
+
+# my_float = 80
+# entrace_number = (my_float-1) // 20 + 1
+# print(entrace_number)
+
+
+# year = 1800
+# if not year % 4 == 0 and year % 100 != 0:
+#     print('year is leap')
+# elif not year % 400:
+#     print('year is leap')
+# else:
+#     print('year is not leap'
+
+# Определяем размер таблицы
+# size = 10
+#
+# # Создаем таблицу умножения размером size x size с использованием одного цикла
+# for i in range(1, size + 1):
+#     # Внутренний цикл, который создает строку таблицы умножения
+#     row = ""
+#     for j in range(1, size + 1):
+#         # Добавляем произведение i и j в строку с выравниванием
+#         row += f"{i * j:4}"
+#     # Печатаем строку таблицы
+#     print(row)
+
+
+
+# names = ['rahat','marlen','asan','dastan']
+# for index,name in enumerate(names):
+#     print(f'Индекс {index}: Имена {name}:')
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# numbers1 = [1,5,5,2,6,10,11,22]
+#
+# def find_average(numbers):
+#     average = sum(numbers) / len(numbers)
+#     return average
+# average1 = find_average(numbers)
+# average2 = find_average(numbers1)
+# print(average1,average2)
+
+
+
+
+
+
+
+
+
